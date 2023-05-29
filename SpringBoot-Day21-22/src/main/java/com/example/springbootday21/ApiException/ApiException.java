a2c531f4374a2ab4040ad573a0955a65c865ab86
SWE - Wid Seraj <120143921+wid21@users.noreply.github.com>
2023-05-29 22:35:41 +0300

    Delete SpringBoot-Day21-22 directory

--- SpringBoot-Day21-22/src/main/java/com/example/springbootday21/ApiException/ApiException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootday21.ApiException;
-
-public class ApiException extends RuntimeException{
-    public ApiException(String message){
-        super(message);
-    }
-}
--- SpringBoot-Day21-22/src/main/java/com/example/springbootday21/Repository/CustomerDetailesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootday21.Repository;
-
-import com.example.springbootday21.Model.CustomerDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerDetailesRepository extends JpaRepository<CustomerDetails,Integer> {
-}
