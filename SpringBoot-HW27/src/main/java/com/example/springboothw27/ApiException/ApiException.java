32d9c9033e3ad9b9b634db840c99cae2a62e19b3
SWE - Wid Seraj <120143921+wid21@users.noreply.github.com>
2023-06-06 01:31:10 +0300

    Delete SpringBoot-HW27 directory

--- SpringBoot-HW27/src/main/java/com/example/springboothw27/ApiException/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboothw27.ApiException;
-
-public class ApiException extends RuntimeException{
-
-     public ApiException(String message){
-         super(message);
-     }
-}
